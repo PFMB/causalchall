2a82c013c723b15fd8e016f85a88ec1042941ee4
Philipp Baumann <ph.baumann@t-online.de>
2022-03-06 17:46:57 +0100

    XGBoost Classifier added to comparison

--- nn_classifier_toy_data.R
+++ nn_classifier_toy_data.R
@@ -1,11 +1,12 @@
+rm(list = ls())
 pacman::p_load("keras","tensorflow", "tfdatasets", "reticulate","dplyr","randomForest","mgcv","ggplot2","stringr","data.table")
 set.seed(1)
 n <- 1000
 x1 <- runif(n)
 x2 <- runif(n)
 x3 <- runif(n)
-x <- matrix(rnorm(n*3), ncol = 3) # noise variables
-y <- plogis(3*x1*x2*x3 + 5*x2^3 - 5*sqrt(x3) + rnorm(n)) > 0.5
+x <- matrix(rnorm(n*30), ncol = 30) # noise variables
+y <- plogis(3*x1*x2*x3 + 3*x2^3 - 5*sqrt(x3) + rnorm(n)) > 0.5
 mean(y)
 d = data.frame(y = as.integer(y),x1,x2,x3,x)
 
@@ -60,7 +61,7 @@ SL.NN_base <- function(Y, X, newX = NULL, family = "binomial") {
   
   model %>% compile(
     loss = ifelse(family == "binomial", "binary_crossentropy", "mse"), #loss_huber(), loss_mean_squared_error(), loss_binary_crossentropy()
-    optimizer = optimizer_rmsprop(learning_rate = 5e-4), #optimizer_rmsprop
+    optimizer = optimizer_rmsprop(learning_rate = 1e-3), #optimizer_rmsprop
     metrics = ifelse(family == "binomial", "accuracy", "mse") # "mse","mae","accuracy"
   )
   
@@ -87,7 +88,8 @@ SL.NN_base <- function(Y, X, newX = NULL, family = "binomial") {
 nn_pred <- SL.NN_base(Y = d[tr_idx,"y"], X = d[tr_idx,] %>% select(-y), newX = d[tst_idx,] %>% select(-y))
 
 ## Classifier 2:
-m <- gam(y ~ s(x1,x2,x3) + s(x2) + s(x3) + X1 + X2 + X3, family = binomial(), data = d[tr_idx,]); summary(m)
+form <- paste0("y ~ s(x1,x2,x3) + s(x2) + s(x3) + ", paste0("X",1:ncol(x), collapse = " + "))
+m <- gam(as.formula(form), family = binomial(), data = d[tr_idx,]); summary(m)
 gam_pred <- predict(m, type = "response", newdata = d[tst_idx,])
 
 ## Classifier 3:
@@ -95,7 +97,57 @@ fit.rf <- randomForest(y = as.factor(d[tr_idx,"y"]), x = d[tr_idx,] %>% select(-
                        ntree = 300, xtest = d[tst_idx,] %>% select(-y), keep.forest = TRUE, importance = TRUE)
 rf_pred <- fit.rf$test$predicted
 
-preds <- data.frame(nn = nn_pred, gam = gam_pred, rf = fit.rf$test$votes[,"1"], true = d[tst_idx, "y"])
+## Classifier 4:
+SL.xgboost_man <- function (Y, X, newX, family, obsWeights, id, ntrees = 1000, 
+                            max_depth = 4, shrinkage = 0.1, minobspernode = 10, params = list(), 
+                            nthread = 1, verbose = 0, save_period = NULL, ...) 
+{
+  #.SL.require("xgboost")
+  if (packageVersion("xgboost") < 0.6) 
+    stop("SL.xgboost requires xgboost version >= 0.6, try help('SL.xgboost') for details")
+  if (!is.matrix(X)) {
+    X = model.matrix(~. - 1, X)
+  }
+  xgmat = xgboost::xgb.DMatrix(data = X, label = Y, weight = obsWeights)
+  if (family$family == "gaussian") {
+    if (packageVersion("xgboost") >= "1.1.1.1") {
+      objective <- "reg:squarederror"
+    }
+    else {
+      objective <- "reg:linear"
+    }
+    model = xgboost::xgboost(data = xgmat, objective = objective, 
+                             nrounds = ntrees, max_depth = max_depth, min_child_weight = minobspernode, 
+                             eta = shrinkage, verbose = verbose, nthread = nthread, 
+                             params = params, save_period = save_period)
+  }
+  if (family$family == "binomial") {
+    model = xgboost::xgboost(data = xgmat, objective = "binary:logistic", 
+                             nrounds = ntrees, max_depth = max_depth, min_child_weight = minobspernode, 
+                             eta = shrinkage, verbose = verbose, nthread = nthread, 
+                             params = params, save_period = save_period, eval_metric = "logloss")
+  }
+  if (family$family == "multinomial") {
+    model = xgboost::xgboost(data = xgmat, objective = "multi:softmax", 
+                             nrounds = ntrees, max_depth = max_depth, min_child_weight = minobspernode, 
+                             eta = shrinkage, verbose = verbose, num_class = length(unique(Y)), 
+                             nthread = nthread, params = params, save_period = save_period)
+  }
+  if (!is.matrix(newX)) {
+    newX = model.matrix(~. - 1, newX)
+  }
+  pred = predict(model, newdata = newX)
+  fit = list(object = model)
+  class(fit) = c("SL.xgboost")
+  out = list(pred = pred, fit = fit)
+  return(out)
+}
+
+famil_y <- list(family = "binomial")
+out_p <- SL.xgboost_man(Y = d[tr_idx,"y"], X = d[tr_idx,] %>% select(-y),
+                        newX = d[tst_idx,] %>% select(-y), family = famil_y, obsWeights = rep(1, length(tr_idx)))
+
+preds <- data.frame(nn = nn_pred, gam = gam_pred, rf = fit.rf$test$votes[,"1"], xgb = out_p$pred, true = d[tst_idx, "y"])
 
 #colMeans(apply(preds, 2, function(x) x == preds$true))
 
@@ -121,7 +173,7 @@ cut_off <- seq(0,1, by = 0.01)
 tpr_fpr <- lapply(cut_off, calc_tpr_fpr, pr = preds)
 tpr_fpr <- do.call("rbind",tpr_fpr)
 rw_names <- rownames(tpr_fpr)
-tpr_fpr$cutoff <- as.character(cut_off)
+tpr_fpr$cutoff <- rep(as.character(cut_off), each = ncol(preds) - 1)
 tpr_fpr$method <- str_extract(rw_names, "[aA-zZ]+")
 setDT(tpr_fpr)
 
@@ -148,10 +200,12 @@ roc_func <- function(fpr_value) {
 }
 
 meth <- "nn"
-round(integrate(Vectorize(roc_func), lower = 0, upper = 1)$value,3) # 0.995
+round(integrate(Vectorize(roc_func), lower = 0, upper = 1, subdivisions = 200L)$value,3) # 0.969
 meth <- "gam"
-round(integrate(Vectorize(roc_func), lower = 0, upper = 1)$value,3) # 0.994
+round(integrate(Vectorize(roc_func), lower = 0, upper = 1, subdivisions = 200L)$value,3) # 0.963
 meth <- "rf"
-round(integrate(Vectorize(roc_func), lower = 0, upper = 1)$value,3) # 0.991
+round(integrate(Vectorize(roc_func), lower = 0, upper = 1, subdivisions = 200L)$value,3) # 0.916
+meth <- "xgb"
+round(integrate(Vectorize(roc_func), lower = 0, upper = 1, subdivisions = 200L)$value,3) # 0.96
 
 
