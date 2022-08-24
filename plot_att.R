2e7f1b67b43f3b81cec633db898aa9c58b7f4125
Philipp Baumann <ph.baumann@t-online.de>
2022-08-24 21:56:35 +0200

    refined plot

--- plot_att.R
+++ plot_att.R
@@ -5,8 +5,8 @@ library(ggplot2)
 # substitute "LogResults" with "Results"
 
 d1 <- fread("LogResults/results_1.csv", header = T)
-d2 <- fread("LogResults/results_2.csv", header = T)
-d <- rbind(d1,d2)
+#d2 <- fread("LogResults/results_2.csv", header = T)
+d <- rbind(d1)
 
 satt_3 <- d[variable == "Overall" & year == "3",]$satt
 satt_4 <- d[variable == "Overall" & year == "4",]$satt
@@ -16,8 +16,8 @@ d <- rbind(data.table(ATT = satt_3, year = "Year 3"),
 
 # labels to plot
 
-att_mean_3 <- round(d[year == "Year 3", mean(ATT)], 2)
-att_mean_4 <- round(d[year == "Year 4", mean(ATT)], 2)
+att_mean_3 <- round(d[year == "Year 3", mean(ATT)], 1)
+att_mean_4 <- round(d[year == "Year 4", mean(ATT)], 1)
 
 l <- list(mean_3 = att_mean_3, mean_4 = att_mean_4)
 
@@ -25,20 +25,20 @@ dat_text <- data.frame(
   label = c(as.character(as.expression(substitute(mu == mean_3, l))), 
             as.character(as.expression(substitute(mu == mean_4, l)))),
   year   = c("Year 3", "Year 4"),
-  x     = c(-40, -40), # position in plot (50, 50) for non-log results
+  x     = c(-20, 0), # position in plot (50, 50) for non-log results
   y     = c(0.02, 0.02),
   stringsAsFactors = F
 )
 
-p <- ggplot(d, aes(ATT)) + geom_density() + 
+(p <- ggplot(d, aes(ATT)) + geom_density() + 
         geom_vline(data = d, aes(xintercept = mean(ATT), group = year), colour = "blue") +
         facet_grid(.~ year) + ylab("Density") +
         geom_text(data    = dat_text, parse = T,
                   mapping = aes(x = x, y = y, label = label),
-                  size = 3) +
-        theme_light() 
+                  size = 4) +
+        theme_light())
 
-ggsave("logresults_causal_challange.pdf")
+ggsave("logresults_causal_challange.pdf", height = 4, width = 10)
 
 # Old plot
 #par(mfrow = c(1,2))
